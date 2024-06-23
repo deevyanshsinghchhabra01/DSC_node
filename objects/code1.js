33190d6eb0312e1d759fc2daaa33108885810413
Deevyansh Singh Chhabra <deevyanshsinghchhabra01@gmail.com>
2024-06-23 14:09:54 +0530

    first commit

---
+++ objects/code1.js
@@ -0,0 +1,11 @@
+// objects in js
+
+// object is ds in js 
+// its like key value pair similar to dictionary in other programing language
+
+const obj = {
+    name:'deevyansh',
+    age: 21,
+    availabe : true
+}
+console.log(obj)
